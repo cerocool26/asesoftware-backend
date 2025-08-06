4e1b92e786619366f160eb25d2275095ab20e419
andres <andrescamiloro26@gmail.com>
2025-08-06 07:45:28 -0500

    arquetipo

---
+++ src/main/java/com/asesoftware/shifts/ShiftsApplication.java
@@ -0,0 +1,13 @@
+package com.asesoftware.shifts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiftsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShiftsApplication.class, args);
+	}
+
+}
